60475f1144431e3789307120d30d229c03133fb8
ClassHacker <Ajaydayma1401@gmail.com>
2022-11-19 17:25:29 +0530

    Fixed test case

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Work in progess.../i);
+  const linkElement = screen.getByText(/Loading.../i);
   expect(linkElement).toBeInTheDocument();
 });
